a21fffc44a84d57bc99328bd7359d947d64a2067
johnnyconroy <jsknap@gmail.com>
2018-01-21 15:12:16 +0100

    change server port

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'pickprank-app',
+    script: './server.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-200-21-207.us-west-2.compute.amazonaws.com',
+      key: '~/.ssh/jsk-pickprank-key-pair.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:johnnyconroy/pickprank.git',
+      path: '/home/ubuntu/pickprank-app',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(function(req, res, next) {
 app.use(express.static("./build"));
 
 
-app.listen(4000);
+app.listen(3000);
 
-console.log("Express app running on port 4000");
+console.log("Express app running on port 3000");
 
 module.exports = app;
 
 // chrome://inspect/#devices
 // node --inspect --debug-brk server.js
-// zip CLI ==> git archive -v -o myapp.zip --format=zip HEAD
+// zip CLI ==> git archive -v -o ../myapp.zip --format=zip HEAD
