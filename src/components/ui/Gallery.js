1c61b177dcfab0d7cea57152110ad3d942d2c09f
johnnyconroy <jsknap@gmail.com>
2018-01-21 17:40:34 +0100

    changed gallery picture

--- src/components/ui/Gallery.js
+++ src/components/ui/Gallery.js
@@ -17,7 +17,7 @@ export const Gallery = ({galleryURLs, authed, requestSignIn}) => {
 	return(
         <div>
         	<FacebookLogin authed={authed} requestSignIn={requestSignIn}/>
-            <p className="dummyTitle"> Gallery Pickpranks</p>
+            <p className="dummyTitle"> Gallery Pictures</p>
             <ul className="gallery-list">{listComponent}</ul>
         </div>
 	)
