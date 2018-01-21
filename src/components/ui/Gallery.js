f991d446b99f531b9661b0b954d6af30d6fab28e
johnnyconroy <jsknap@gmail.com>
2018-01-21 17:08:17 +0100

    test pm2 deployment

--- src/components/ui/Gallery.js
+++ src/components/ui/Gallery.js
@@ -17,7 +17,7 @@ export const Gallery = ({galleryURLs, authed, requestSignIn}) => {
 	return(
         <div>
         	<FacebookLogin authed={authed} requestSignIn={requestSignIn}/>
-            <p className="dummyTitle"> Gallery </p>
+            <p className="dummyTitle"> Gallery Pickpranks</p>
             <ul className="gallery-list">{listComponent}</ul>
         </div>
 	)
